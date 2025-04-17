b7e125a5dbc6162fd7fb6e98b88375526529bd31
prof218 <profwork218@gmail.com>
2025-04-17 11:00:55 +0530

    first reacrt

---
+++ src/content_option.js
@@ -0,0 +1,202 @@
+const logotext = "RONAK";
+const meta = {
+    title: "Ronak Patel",
+    description: "I’m Ronak Patel and Full stack devloper,currently working in Gujarat, India.",
+};
+
+const introdata = {
+    title: "I’m Ronak Patel",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop Websites",
+    },
+    description: "Experienced Full Stack Developer with 5 years of expertise in designing, building, and maintaining efficient web applications.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Experienced Full Stack Developer with 5 years of expertise in designing, building, and maintaining efficient web applications. Proficient in front-end development with React and Next, and back-end development using Node.js and Express.js. Skilled in integrating with various databases like MongoDB, MySQL, PostgreSQL, and SQL Server. Adept at ensuring high performance, quality, and responsiveness of applications. Proven track record of collaborating with cross-functional teams and conducting code reviews to maintain high standards. Excels in troubleshooting and optimizing web pages for maximum speed and scalability.",
+};
+const worktimeline = [{
+        jobtitle: "Junior Frontend Developer",
+        where: "Sakhi Infotech, Surat - GJ, India",
+        date: "2018 - 2020",
+    },
+    {
+        jobtitle: "Senior Frontend Developer",
+        where: "Redjinni It Services",
+        date: "2021 - 2023",
+    },
+    {
+        jobtitle: "Full Stack Web Developer",
+        where: "Script-Jet It Services",
+        date: "2023 - 2024",
+    },
+];
+
+const skills = [{
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Next",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "Typescript",
+        value: 90,
+    },
+    {
+        name: "Node",
+        value: 90,
+    },
+    {
+        name: "Express",
+        value: 85,
+    },
+    {
+        name: "MongoDb",
+        value: 85,
+    },
+    {
+        name: "Postgres-Sql",
+        value: 85,
+    },
+];
+
+const services = [
+    {
+        title: "Website Design",
+        description: "Crafting visually stunning, user-friendly, and responsive websites to effectively showcase your brand and engage your audience.",
+    },
+    {
+        title: "WordPress Design",
+        description: "Creating customized WordPress websites with professional themes, plugins, and functionality tailored to your business needs.",
+    },
+    {
+        title: "Website Maintenance",
+        description: "Providing regular updates, security checks, and performance optimizations to ensure your website runs smoothly and efficiently.",
+    },
+    {
+        title: "eCommerce Development",
+        description: "Building scalable and secure online stores with seamless payment integration, product management, and an engaging shopping experience.",
+    },
+    {
+        title: "Gaming Website Development",
+        description: "Designing immersive and interactive gaming platforms with high performance, responsive designs, and intuitive user interfaces.",
+    },
+    {
+        title: "SEO Optimization",
+        description: "Optimizing your website for search engines to improve rankings, drive traffic, and enhance online visibility.",
+    },
+    {
+        title: "Custom Web Applications",
+        description: "Developing tailored web applications to meet unique business needs with cutting-edge technology and innovative solutions.",
+    },
+    {
+        title: "UI/UX Design",
+        description: "Designing intuitive and engaging interfaces to deliver exceptional user experiences across web and mobile platforms.",
+    },
+    {
+        title: "Mobile-Friendly Design",
+        description: "Creating responsive designs optimized for mobile devices, ensuring a seamless experience for users on the go.",
+    },
+];
+
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "profwork218@gmail.com",
+    YOUR_FONE: "+91 6352751091",
+    description: "We’d love to hear from you! Whether you have a question, need assistance, or want to discuss your next project, our team is here to help. Reach out to us through the form below, email us directly, or give us a call. Let’s create something amazing together! ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/ronakinfy007",
+    linkedin: "https://www.linkedin.com/in/ronak-p-85b687170/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
